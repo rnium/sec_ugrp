80f4907c9bab11c6cb1398e3270a172123f84c72
rony <rniumo@gmail.com>
2023-08-02 15:50:32 +0600

    form validators

--- results/static/results/scripts/viewsemester.js
+++ results/static/results/scripts/viewsemester.js
@@ -19,7 +19,72 @@ function showDevModal(id) {
     mBootstrap.show()
 }
 
-// showDevModal("dropCourseModal")
+showDevModal("newEntryModal")
 
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+
+function getNewCourseData() {
+    let courseCodeIn = $("#courseCodeInput").val().trim();
+    let courseTitleIn = $("#courseTitleInput").val().trim();
+    let totalMarksIn = parseInt($("#totalMarksInput").val().trim());
+    let creditsIn = parseInt($("#courseCreditsInput").val().trim());
+    let partAMarksIn = parseInt($("#partAmarksInput").val().trim());
+    let partBMarksIn = parseInt($("#partBmarksInput").val().trim());
+    let incourseMarksIn = parseInt($("#inCourseMarksInput").val().trim());
+    
+    let courseCodeArray = courseCodeIn.split(" ")
+    let courseCodeNumber = parseInt(courseCodeArray[1])
+    
+    if (isNaN(totalMarksIn) | isNaN(creditsIn) | isNaN(partAMarksIn) | isNaN(partBMarksIn | isNaN(incourseMarksIn))) {
+        $("#createCourseAlert").text("Invalid Input(s), please fill correctly");
+        $("#createCourseAlert").show()
+        return false;
+    }
+
+    if (courseCodeIn.length == 0 | courseTitleIn.length == 0) {
+        $("#createCourseAlert").text("Please fill all the fields");
+        $("#createCourseAlert").show()
+        return false;
+    }
+    if (courseCodeArray.length != 2 | isNaN(courseCodeNumber)) {
+        $("#createCourseAlert").text("Invalid Course code! Please enter correctly.");
+        $("#createCourseAlert").show()
+        return false;
+    }
+    let semesterCodeOfCC = parseInt(courseCodeNumber.toString()[0])
+    if (semesterNthNumber != semesterCodeOfCC) {
+        $("#createCourseAlert").text("Invalid Course code for this semester!");
+        $("#createCourseAlert").show()
+        return false;
+    }
+    if ((partAMarksIn+partBMarksIn+incourseMarksIn) > totalMarksIn) {
+        $("#createCourseAlert").text("Invalid Marks Distribution!");
+        $("#createCourseAlert").show()
+        return false;
+    }
+    else {
+        $("#createCourseAlert").hide()
+    }
+    
+    data = {
+        "semester": semesterId,
+        "code": courseCodeIn,
+        "title": courseTitleIn,
+        "course_credit": creditsIn,
+        "total_marks": totalMarksIn,
+        "part_A_marks": partAMarksIn,
+        "part_B_marks": partBMarksIn,
+        "incourse_marks": incourseMarksIn,
+        "added_by": userId,
+    }
+
+    return data;
+}
+
+$(document).ready(function () {
+    $("#createCourseAddBtn").on('click', ()=>{
+        console.log(getNewCourseData());
+    })
+});
