ffccea7f91c0c74834e68233227ef1dc851282b1
rony <rniumo@gmail.com>
2023-08-08 00:43:01 +0600

    login api

---
+++ account/static/account/scripts/login.js
@@ -0,0 +1,85 @@
+function getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+
+const csrftoken = getCookie('csrftoken');
+
+function showError(msg, duration=3000) {
+    $("#error-info-text").text(msg)
+    $("#error-info-text").show(0, function(){
+        setTimeout(function(){
+            $("#error-info-text").text(" ")
+            $("#error-info-text").hide()
+        }, duration)
+    })
+}
+
+function checkForm() {
+    let input_fields = $(".form-control")
+    for (let field of input_fields) {
+        field_val = $(`#${field.id}`).val().trim()
+        if (field_val.length < 1) {
+            showError("Please fill the field")
+            $(`#${field.id}`).focus()
+            return false
+        }
+    }
+    return true
+}
+
+function submitForm() {
+    let form_is_valid = checkForm()
+    if (!form_is_valid) {
+        return false
+    }
+    let data = {
+        "email": $("#emailInput").val().trim(),
+        "password": $("#passwordInput").val().trim()
+    }
+    let payload = JSON.stringify(data)
+    $.ajax({
+        type: "post",
+        url: login_url,
+        dataType: "json",
+        contentType: "application/json",
+        beforeSend: function(xhr){
+            // $("#id_create_q_button").attr("disabled", true)
+            xhr.setRequestHeader("X-CSRFToken", csrftoken)
+        },
+        data: payload,
+        cache: false,
+        success: function(response) {
+            window.location = response['succes_url']
+        },
+        error: function(xhr, error, status) {
+            showError("Booo..")
+            console.log(xhr);
+
+        }
+    });
+
+}
+
+
+$(document).ready(function () {
+    $("#password").on('keyup', function (e) { 
+        if (e.key == 'Enter' || e.keyCode === 13) {
+            submitForm()
+        }
+     })
+    $("#login-btn").on('click', submitForm)
+});
+
+
--- account/views.py
+++ account/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, redirect
+from django.urls import reverse
 from django.views import View
-from django.contrib.auth import login, logout
+from django.contrib.auth import login, logout, authenticate
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST, HTTP_401_UNAUTHORIZED
 
 
 def login_page(request):
@@ -14,3 +18,13 @@ class LogoutView(View):
         if request.user.is_authenticated:
             logout(request)
         return redirect("account:user_login_get")
+    
+@api_view(['POST'])
+def api_login(request):
+    user = authenticate(username=request.data['email'], password=request.data['password'])
+    if user:
+        login(request, user)
+        success_url = reverse("results:dashboard")
+        return Response({'status':'logged in', 'succes_url': success_url}, status=HTTP_200_OK)
+    else:
+        return Response({'status':'login failed'}, status=HTTP_401_UNAUTHORIZED)
