fd9b612d5fd0df5cc7261b1a00aeb1951d201fbb
rony <rniumo@gmail.com>
2023-08-08 00:19:01 +0600

    account login, logout

--- account/views.py
+++ account/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views import View
+from django.contrib.auth import login, logout
 
-# Create your views here.
+
+def login_page(request):
+    if request.user.is_authenticated:
+            return redirect("results:dashboard")
+    else:
+        return render(request=request, template_name='account/login_page.html')
+    
+class LogoutView(View):
+    def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            logout(request)
+        return redirect("account:user_login_get")
--- config/settings.py
+++ config/settings.py
@@ -128,6 +128,8 @@ STATIC_ROOT = BASE_DIR/'static'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR/'media'
 
+LOGIN_URL = '/account/login/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
