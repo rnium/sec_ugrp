336ed4fad940bd4170582bd205db6ee3f6c96464
rony <rniumo@gmail.com>
2023-08-08 16:23:39 +0600

    local settings

--- config/settings.py
+++ config/settings.py
@@ -134,3 +134,8 @@ LOGIN_URL = '/account/login/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
