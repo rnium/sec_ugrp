8e9a231388a578d239d65d5ec2c2eeb781cb34d0
rony <rniumo@gmail.com>
2023-08-14 21:53:23 +0600

    navbar active by default

--- results/context_processors.py
+++ results/context_processors.py
@@ -1,5 +1,5 @@
 def navbarContext(request):
-    nav = request.COOKIES.get("nav", "")
+    nav = request.COOKIES.get("nav", "active")
     context = {}
     if nav == "active":
         context['is_navActive'] = True
