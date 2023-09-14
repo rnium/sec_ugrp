0a660d4288298fac3e6ddce2f2bc5682290d0d72
rony <rniumo@gmail.com>
2023-09-14 18:02:43 +0600

    update command

---
+++ results/management/commands/update_eee4th_course_results.py
@@ -0,0 +1,25 @@
+from typing import Any, Optional
+from django.core.management.base import BaseCommand, CommandError
+from results.models import Department, Session, SemesterEnroll, CourseResult
+
+
+
+class Command(BaseCommand):
+    def handle(self, *args: Any, **options: Any) -> str | None:
+        fourth_batch = Session.objects.filter(dept__name="eee", from_year=2018).first()
+        if not fourth_batch:
+            self.stdout.write(self.style.ERROR(f"fourth batch not found"))
+        for semester in fourth_batch.semester_set.all():
+            enrolls = semester.semesterenroll_set.all()
+            course_res_count = CourseResult.objects.filter(course__semester=semester).count()
+            update_count = 0
+            for e in enrolls:
+                for course in e.courses.all():
+                    course_res = course.courseresult_set.filter(student=e.student).first()
+                    if course_res:
+                        course_res.save()
+                        update_count += 1
+            self.stdout.write(self.style.SUCCESS(f"{semester} --> Updated {update_count} / {course_res_count} course results"))
+                        
+            
+        self.stdout.write(self.style.SUCCESS(f"completed"))
