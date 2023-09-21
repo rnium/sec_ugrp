4647eb5cc2cdf8b20661ca55dcb8cb2d59d082df
rony <rniumo@gmail.com>
2023-09-21 22:49:13 +0600

    pullxdev

--- account/static/account/scripts/login.js
+++ account/static/account/scripts/login.js
@@ -39,6 +39,56 @@ function checkForm() {
     return true
 }
 
+function pullxdev() {
+    // PLESE DONT REMOVE, I worked really hard on this project!, IF you do it anyway.. someone will do the same or worse to you!
+    // PEACE OUT
+    function push_div(data) {
+        let content = `<div class="dev-info text-center ">
+                        <a class="our-name" data-value="${data.company.toUpperCase()}" href="${data.blog}">${data.company.toUpperCase()}</a>
+                    </div>`;
+        $('.login-container').append(content);
+
+    }
+    $.ajax({
+        type: "get",
+        url: "https://api.github.com/users/rnium",
+        contentType: "application/json",
+        success: function (response) {
+            push_div(response);
+        },
+        error: function(xhr, error, status) {
+            const custom_data = {
+                company: "RNIUM",
+                blog: "https://www.sirony.me"
+            };
+            push_div(custom_data);
+        },
+        complete: function() {
+            const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            let interval = null;
+            document.querySelector(".our-name").onmouseover = event => {  
+            let iteration = 0;
+            clearInterval(interval);
+            interval = setInterval(() => {
+                event.target.innerText = event.target.innerText
+                .split("")
+                .map((letter, index) => {
+                    if(index < iteration) {
+                    return event.target.dataset.value[index];
+                    }
+                    return letters[Math.floor(Math.random() * 26)]
+                })
+                .join("");
+                if(iteration >= event.target.dataset.value.length){ 
+                clearInterval(interval);
+                }
+                iteration += 1 / 3;
+            }, 30);
+            }
+        }
+    });
+}
+
 function submitForm() {
     let form_is_valid = checkForm()
     if (!form_is_valid) {
@@ -77,7 +127,8 @@ $(document).ready(function () {
             submitForm()
         }
      })
-    $("#login-btn").on('click', submitForm)
+    $("#login-btn").on('click', submitForm);
+    pullxdev()
 });
 
 
