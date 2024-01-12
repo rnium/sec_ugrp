c83dc261b39fa7f1afbab96f41d40390be75f716
rony <rniumo@gmail.com>
2024-01-12 20:23:02 +0600

    dept restore id reuse

--- results/tasks.py
+++ results/tasks.py
@@ -8,8 +8,8 @@ import time
 @shared_task(bind=True)
 def restore_dept_data_task(self, dept_id, sessions_data, total_objects):
     dept = Department.objects.get(id=dept_id)
-    semester_drop_courses_prev_IDs = {}
-    enrollment_courses_prev_IDs = {}
+    semester_drop_courses_IDs = {}
+    enrollment_courses_IDs = {}
     semester_hash = {}        # key = previous, value = new object
     enrollment_hash = {}        # key = previous, value = new object
     courses_hash = {}       # key = previous, value = new object
@@ -20,7 +20,6 @@ def restore_dept_data_task(self, dept_id, sessions_data, total_objects):
     count = 0
     progress_recorder.set_progress(count, total_objects)
     for session_data in sessions_data:
-        session_data['session_meta'].pop('id')
         session_data['session_meta']['dept'] = dept
         session = Session(**session_data['session_meta'])
         session.save()
@@ -35,9 +34,8 @@ def restore_dept_data_task(self, dept_id, sessions_data, total_objects):
             progress_recorder.set_progress(count, total_objects)
         # Semesters
         for sem_data in session_data['semesters']:
-            prev_sem_id = sem_data['semester_meta']['id']
-            semester_drop_courses_prev_IDs[prev_sem_id] = sem_data['semester_meta']['drop_courses']
-            sem_data['semester_meta'].pop('id')
+            sem_id = sem_data['semester_meta']['id']
+            semester_drop_courses_IDs[sem_id] = sem_data['semester_meta']['drop_courses']
             sem_data['semester_meta'].pop('drop_courses')
             sem_data['semester_meta']['session'] = session
             if sem_data['semester_meta']['added_by']:
@@ -50,10 +48,9 @@ def restore_dept_data_task(self, dept_id, sessions_data, total_objects):
             semester.save()
             count += 1
             progress_recorder.set_progress(count, total_objects)
-            semester_hash[prev_sem_id] = semester
+            semester_hash[sem_id] = semester
             # Courses
             for course_data in sem_data['courses']:
-                course_data['course_meta'].pop('id')
                 course_data['course_meta']['semester'] = semester
                 if course_data['course_meta']['added_by']:
                     user = User.objects.filter(username=course_data['course_meta']['added_by']).first()
@@ -65,11 +62,10 @@ def restore_dept_data_task(self, dept_id, sessions_data, total_objects):
                 course.save()
                 count += 1
                 progress_recorder.set_progress(count, total_objects)
-                courses_hash[course.identifier] = course
+                courses_hash[course.id] = course
                 # Course Results
                 for result_data in course_data['course_results']:
                     result_id = result_data['id']
-                    result_data.pop('id')
                     result_data['course'] = course
                     try:
                         result_data['student'] = StudentAccount.objects.get(registration=result_data['student'])
@@ -78,19 +74,18 @@ def restore_dept_data_task(self, dept_id, sessions_data, total_objects):
                         continue
                     retake_of_id = result_data['retake_of']
                     result_data.pop('retake_of')
-                    course_res_new = CourseResult(**result_data)
-                    course_res_new.save()
-                    courseresults_hash[result_id] = course_res_new
+                    course_res = CourseResult(**result_data)
+                    course_res.save()
+                    courseresults_hash[result_id] = course_res
                     if retake_of_id:
-                        retake_hash[course_res_new] = retake_of_id
+                        retake_hash[course_res] = retake_of_id
                     count += 1
                 progress_recorder.set_progress(count, total_objects)
                 time.sleep(0.2)
             # Enrolls
             for enroll_data in sem_data['enrolls']:
-                enrollment_prev_id = enroll_data['id']
-                enrollment_courses_prev_IDs[enrollment_prev_id] = enroll_data['courses']
-                enroll_data.pop('id')
+                enrollment_id = enroll_data['id']
+                enrollment_courses_IDs[enrollment_id] = enroll_data['courses']
                 enroll_data.pop('courses')
                 enroll_data['student'] = StudentAccount.objects.get(registration=enroll_data['student'])
                 enroll_data['semester'] = semester
@@ -98,37 +93,35 @@ def restore_dept_data_task(self, dept_id, sessions_data, total_objects):
                 enroll.save()
                 count += 1
                 progress_recorder.set_progress(count, total_objects)
-                enrollment_hash[enrollment_prev_id] = enroll
+                enrollment_hash[enrollment_id] = enroll
     # retry failed courseresult creation 
     for result_data in failed_course_results:
         result_data['student'] = StudentAccount.objects.get(registration=result_data['student'])
         retake_of_id = result_data['retake_of']
         result_data.pop('retake_of')
-        course_res_new = CourseResult(**result_data)
-        course_res_new.save()
-        courseresults_hash[result_id] = course_res_new
+        course_res = CourseResult(**result_data)
+        course_res.save()
+        courseresults_hash[result_id] = course_res
         if retake_of_id:
-            retake_hash[course_res_new] = retake_of_id
+            retake_hash[course_res] = retake_of_id
         count += 1
     progress_recorder.set_progress(count, total_objects)
-    time.sleep(0.2)
     # Binding Retakings
-    for new_result, retake_of_id in retake_hash.items():
-        new_result.retake_of = courseresults_hash[retake_of_id]
-        new_result.save()
+    for course_res, retake_of_id in retake_hash.items():
+        course_res.retake_of = courseresults_hash[retake_of_id]
+        course_res.save()
     # semester drop courses
-    for prevId, newSem in semester_hash.items():
-        drop_courses = semester_drop_courses_prev_IDs[prevId]
-        for prev_course_id in drop_courses:
-            newSem.drop_courses.add(courses_hash[prev_course_id])
+    for sem_id, newSem in semester_hash.items():
+        drop_courses = semester_drop_courses_IDs[sem_id]
+        for course_id in drop_courses:
+            newSem.drop_courses.add(courses_hash[course_id])
         count += len(drop_courses)
         progress_recorder.set_progress(count, total_objects)
-    time.sleep(0.2)
     # enrolled courses
-    for prevId, newEnroll in enrollment_hash.items():
-        enrolled_courses = enrollment_courses_prev_IDs[prevId]
-        for prev_course_id in enrolled_courses:
-            newEnroll.courses.add(courses_hash[prev_course_id])
+    for enroll_id, newEnroll in enrollment_hash.items():
+        enrolled_courses = enrollment_courses_IDs[enroll_id]
+        for course_id in enrolled_courses:
+            newEnroll.courses.add(courses_hash[course_id])
         count += len(enrolled_courses)
         progress_recorder.set_progress(count, total_objects)
     
@@ -196,11 +189,11 @@ def restore_session_data_task(self, dept_id, session_data, total_objects):
                 result_data['student'] = StudentAccount.objects.get(registration=result_data['student'])
                 retake_of_id = result_data['retake_of']
                 result_data.pop('retake_of')
-                course_res_new = CourseResult(**result_data)
-                course_res_new.save()
-                courseresults_hash[result_id] = course_res_new
+                course_res = CourseResult(**result_data)
+                course_res.save()
+                courseresults_hash[result_id] = course_res
                 if retake_of_id:
-                    retake_hash[course_res_new] = retake_of_id
+                    retake_hash[course_res] = retake_of_id
                 count += 1
             progress_recorder.set_progress(count, total_objects)                    
         # Enrolls
